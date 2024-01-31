25426fd5e5367fe094c3e16d916996f76ad081a9
Maxim Piratorov <piratorov@gmail.com>
2024-01-31 14:33:20 +0200

    Initial Commit

---
+++ qwizz/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  qwizz
+//
+//  Created by gost on 29.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            CardView(isFaseUP: true)
+            CardView()
+            CardView(isFaseUP: true)
+            CardView()
+        }
+        .foregroundColor(.orange)
+        .padding()
+    }
+}
+
+
+
+struct CardView: View {
+    var isFaseUP: Bool = false
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaseUP {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qwizz/qwizzApp.swift
@@ -0,0 +1,17 @@
+//
+//  qwizzApp.swift
+//  qwizz
+//
+//  Created by gost on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct qwizzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
